2058c6dab9eb6e00da3cda586b43442fbbb49a77
Jonatas Brandão <jonbranaraujo18@gmail.com>
2020-10-14 17:05:06 -0300

    feat: create controllers

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    openingHours,
-    openOnWeekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    openingHours,
-    openOnWeekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333);
